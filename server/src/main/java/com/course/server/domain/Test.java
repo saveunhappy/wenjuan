9dda35f405a243bd8c317b268b2312ec614d2dd8
JiangTaoHou <925147954@qq.com>
2022-01-01 19:16:43 +0800

    1.集成mybatis-generator 步骤1：添加idea插件，就是在pom中的build>plugins>plugin 步骤2:添加server\src\main\resources\generator\generatorConfig.xml 步骤3：创建maven启动命令mybatis-generator:generate -e

--- server/src/main/java/com/course/server/domain/Test.java
+++ server/src/main/java/com/course/server/domain/Test.java
@@ -1,14 +1,17 @@
 package com.course.server.domain;
 
 public class Test {
-    private String id;
+    private Long id;
+
     private String name;
 
-    public String getId() {
+    private String password;
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -18,5 +21,26 @@ public class Test {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", password=").append(password);
+        sb.append("]");
+        return sb.toString();
     }
 }
--- server/src/main/java/com/course/server/mapper/TestMapper.java
+++ server/src/main/java/com/course/server/mapper/TestMapper.java
@@ -1,11 +1,30 @@
 package com.course.server.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
 import com.course.server.domain.Test;
-
+import com.course.server.domain.TestExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
-@Mapper
 public interface TestMapper {
-    List<Test> list();
+    long countByExample(TestExample example);
+
+    int deleteByExample(TestExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
 }
