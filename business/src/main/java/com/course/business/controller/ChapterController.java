66897857cbf8efd1e8bbb7fe27df3aa6e313c63d
JiangTaoHou <925147954@qq.com>
2022-02-18 15:04:41 +0800

    增加大章列表查询接口

---
+++ business/src/main/java/com/course/business/controller/ChapterController.java
@@ -0,0 +1,20 @@
+package com.course.business.controller;
+
+import com.course.server.domain.Chapter;
+import com.course.server.service.ChapterService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class ChapterController {
+    @Resource
+    private ChapterService chapterService;
+    @RequestMapping("/chapter")
+    public List<Chapter> chapter(){
+        List<Chapter> list = chapterService.list();
+        return list;
+    }
+}
