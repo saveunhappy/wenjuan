096552fa380c08de81f4f48a381da7fa107a85b1
JiangTaoHou <925147954@qq.com>
2022-02-19 16:48:36 +0800

    通用的sidebar点击激活样式方法,使用watch监听路由变化

--- admin/src/view/admin.vue
+++ admin/src/view/admin.vue
@@ -474,6 +474,24 @@
       $("body").removeClass("login-layout light-login");
       $("body").attr("class", "no-skin");
     },
+    watch: {
+      $route: {
+        handler:function(val, oldVal){
+          // sidebar激活样式方法二
+          console.log("---->页面跳转：", val, oldVal);
+          let _this = this;
+
+          // if (!_this.hasResourceRouter(val.name)) {
+          //   _this.$router.push("/login");
+          //   return;
+          // }
+
+          _this.$nextTick(function(){  //页面加载完成后执行
+            _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
+          })
+        }
+      }
+    },
     methods:{
       login(){
         this.$router.push("/admin")
