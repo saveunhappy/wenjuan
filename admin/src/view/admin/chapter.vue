d5c05cf5fe1f55f24794eaf09d552ca24ffeeebe
JiangTaoHou <925147954@qq.com>
2022-02-20 18:13:38 +0800

    1.分页参数前后端交互，axios的post请求默认是以流的方式传递参数，所以Controller里的参数要加@RequestBody注解

--- admin/src/view/admin/chapter.vue
+++ admin/src/view/admin/chapter.vue
@@ -79,6 +79,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "chapter",
   data: function () {
@@ -95,11 +97,24 @@ export default {
   methods: {
     list() {
       let _this = this;
-      _this.$ajax.get("http://127.0.0.1:9000/business/admin/chapter/list").then((response) => {
+      _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/list",
+      {
+        page:1,
+            size:1
+      }).then((response) => {
         console.log("查询大章列表结果", response);
-        _this.chapters = response.data;
+        _this.chapters = response.data.list;
       })
+      // axios.post("http://127.0.0.1:9000/business/admin/chapter/list",
+      //     {
+      //       page:1,
+      //       size:1
+      //     }).then((response) => {
+      //     console.log("查询大章列表结果", response);
+      //     _this.chapters = response.data.list;
+      // })
     }
+
   }
 }
 </script>
--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,14 +1,12 @@
 package com.course.business.controller.admin;
 
-import com.course.server.domain.Chapter;
-import com.course.server.domain.ChapterExample;
-import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.service.ChapterService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin/chapter")
@@ -16,9 +14,8 @@ public class ChapterController {
     @Resource
     private ChapterService chapterService;
     @RequestMapping("/list")
-    public List<ChapterDto> chapter(){
-        List<ChapterDto> list = chapterService.list();
-
-        return list;
+    public PageDto chapter(@RequestBody PageDto pageDto){
+        chapterService.list(pageDto);
+        return pageDto;
     }
 }
