65cf6fd3901f68b98ee38b58b2350f7cc5d7472d
JiangTaoHou <925147954@qq.com>
2022-02-20 13:09:15 +0800

    集成分页插件PageHelper,注意页码从1开始

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -4,6 +4,7 @@ import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
     public List<ChapterDto> list(){
+        PageHelper.startPage(1,1);
         ChapterExample chapterExample = new ChapterExample();
         chapterExample.setOrderByClause("id desc");;
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
