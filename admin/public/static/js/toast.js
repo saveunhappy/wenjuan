734f9d96143128708b64839d7ac7c410b865e0de
JiangTaoHou <925147954@qq.com>
2022-02-22 00:21:59 +0800

    1.修改Toast组件的显示效果,更大气，制作Confirm组件

--- admin/public/static/js/toast.js
+++ admin/public/static/js/toast.js
@@ -1,32 +1,63 @@
-const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-})
+// const Toast = Swal.mixin({
+//     toast: true,
+//     position: 'top-end',
+//     showConfirmButton: false,
+//     timer: 3000,
+//     timerProgressBar: true,
+//     didOpen: (toast) => {
+//         toast.addEventListener('mouseenter', Swal.stopTimer)
+//         toast.addEventListener('mouseleave', Swal.resumeTimer)
+//     }
+// })
 
-toast = {
+// toast = {
+//     success:function (message){
+//         Toast.fire({
+//             icon: 'success',
+//             title: message
+//         })
+//     },
+//     error:function (message){
+//         Toast.fire({
+//             icon: 'error',
+//             title: message
+//         })
+//     },
+//     warning:function (message){
+//         Toast.fire({
+//             icon: 'warning',
+//             title: message
+//         })
+//     },
+// }
+
+
+Toast = {
     success:function (message){
-        Toast.fire({
+        Swal.fire({
+            position: 'top-end',
             icon: 'success',
-            title: message
+            title: message,
+            showConfirmButton: false,
+            timer: 1500
         })
     },
     error:function (message){
-        Toast.fire({
+        Swal.fire({
+            position: 'top-end',
             icon: 'error',
-            title: message
+            title: message,
+            showConfirmButton: false,
+            timer: 1500
         })
     },
     warning:function (message){
-        Toast.fire({
+        Swal.fire({
+            position: 'top-end',
             icon: 'warning',
-            title: message
+            title: message,
+            showConfirmButton: false,
+            timer: 1500
         })
     },
 }
--- admin/src/view/admin/chapter.vue
+++ admin/src/view/admin/chapter.vue
@@ -134,35 +134,47 @@ export default {
         if (resp.success) {
           $("#form-modal").modal('hide');
           _this.list(1)
-          toast.success("保存成功");
+          Toast.success("保存成功");
         }
       })
     },
     del(id) {
       let _this = this;
+      Confirm.show("删除大章后不可恢复!",function () {
+        Loading.show();
+        _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/delete/" + id).then((response) => {
+          console.log("删除大章列表结果", response);
+          let resp = response.data;
+          if (resp.success) {
+            Toast.success("删除成功");
+            _this.list(1);
 
-      Swal.fire({
-        title: '确认删除?',
-        text: "删除后不可恢复!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: '确认删除'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/delete/" + id).then((response) => {
-            console.log("删除大章列表结果", response);
-            let resp = response.data;
-            if (resp.success) {
-              toast.success("删除成功");
-              _this.list(1)
+          }
+        })
+    });
 
-            }
-          })
-
-        }
-      })
+      // Swal.fire({
+      //   title: '确认删除?',
+      //   text: "删除后不可恢复!",
+      //   icon: 'warning',
+      //   showCancelButton: true,
+      //   confirmButtonColor: '#3085d6',
+      //   cancelButtonColor: '#d33',
+      //   confirmButtonText: '确认删除'
+      // }).then((result) => {
+      //   if (result.isConfirmed) {
+      //     _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/delete/" + id).then((response) => {
+      //       console.log("删除大章列表结果", response);
+      //       let resp = response.data;
+      //       if (resp.success) {
+      //         Toast.success("删除成功");
+      //         _this.list(1)
+      //
+      //       }
+      //     })
+      //
+      //   }
+      // })
 
 
     }
