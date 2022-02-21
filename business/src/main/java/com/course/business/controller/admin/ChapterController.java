741f05d00211b1dcd255e7c18887e091c291c465
JiangTaoHou <925147954@qq.com>
2022-02-21 23:31:28 +0800

    1.增加修改大章功能，新增和修改用同一个保存功能，通过传入的参数id有没有值来判断

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -4,6 +4,7 @@ import com.course.server.dto.ChapterDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.ChapterService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,18 +16,19 @@ import javax.annotation.Resource;
 public class ChapterController {
     @Resource
     private ChapterService chapterService;
-    @RequestMapping("/list")
+    @PostMapping("/list")
     public ResponseDto chapter(@RequestBody PageDto pageDto){
         ResponseDto responseDto = new ResponseDto();
         chapterService.list(pageDto);
         responseDto.setContent(pageDto);
         return responseDto;
     }
-    @RequestMapping("/save")
+    @PostMapping("/save")
     public ResponseDto save(@RequestBody ChapterDto chapterDto){
         ResponseDto responseDto = new ResponseDto();
         chapterService.save(chapterDto);
         responseDto.setContent(chapterDto);
         return responseDto;
     }
+
 }
