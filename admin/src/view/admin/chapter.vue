b1b5d13e3ac3eca6a12d60e4cb3d7d25c73ffc6f
JiangTaoHou <925147954@qq.com>
2022-02-24 12:53:27 +0800

    1.后端地址改为读环境变量，修改前端拦截器，之前复制的后面章节的，全部错了。

--- admin/src/view/admin/chapter.vue
+++ admin/src/view/admin/chapter.vue
@@ -113,7 +113,7 @@ export default {
     list(page) {
       let _this = this;
       Loading.show();
-      _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/list",
+      _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/chapter/list",
           {
             page: page,
             size: _this.$refs.pagination.size
@@ -133,7 +133,7 @@ export default {
       //     || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
       //   return;
       // }
-      _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/save",
+      _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/chapter/save",
           _this.chapter).then((response) => {
         console.log("查询大章列表结果", response);
         let resp = response.data;
@@ -151,7 +151,7 @@ export default {
       let _this = this;
       Confirm.show("删除大章后不可恢复!",function () {
         Loading.show();
-        _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/delete/" + id).then((response) => {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/chapter/delete/" + id).then((response) => {
           console.log("删除大章列表结果", response);
           let resp = response.data;
           if (resp.success) {
