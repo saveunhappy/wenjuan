4e5ef1a1b7b83b7b477fd859c8c22e103f71068b
JiangTaoHou <925147954@qq.com>
2022-02-25 13:19:14 +0800

    1.增加枚举常量

--- admin/src/view/admin/section.vue
+++ admin/src/view/admin/section.vue
@@ -15,21 +15,21 @@
     <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
       <tr>
-         <th>ID</th>
+        <th>ID</th>
 
-         <th>标题</th>
+        <th>标题</th>
 
-         <th>课程</th>
+        <th>课程</th>
 
-         <th>大章</th>
+        <th>大章</th>
 
-         <th>视频</th>
+        <th>视频</th>
 
-         <th>时长</th>
+        <th>时长</th>
 
-         <th>收费</th>
+        <th>收费</th>
 
-         <th>顺序</th>
+        <th>顺序</th>
 
 
         <th>操作</th>
@@ -39,14 +39,14 @@
 
       <tbody>
       <tr v-for="section in sections">
-              <td>{{section.id}}</td>
-              <td>{{section.title}}</td>
-              <td>{{section.courseId}}</td>
-              <td>{{section.chapterId}}</td>
-              <td>{{section.video}}</td>
-              <td>{{section.time}}</td>
-              <td>{{CHARGE | optionKV(section.charge)}}</td>
-              <td>{{section.sort}}</td>
+        <td>{{ section.id }}</td>
+        <td>{{ section.title }}</td>
+        <td>{{ section.courseId }}</td>
+        <td>{{ section.chapterId }}</td>
+        <td>{{ section.video }}</td>
+        <td>{{ section.time }}</td>
+        <td>{{ CHARGE | optionKV(section.charge) }}</td>
+        <td>{{ section.sort }}</td>
         <td>
 
           <div class="hidden-sm hidden-xs btn-group">
@@ -72,50 +72,50 @@
           </div>
           <div class="modal-body">
             <form class="form-horizontal">
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">标题</label>
-                  <div class="col-sm-10">
-                    <input v-model="section.title" class="form-control">
-                  </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">标题</label>
+                <div class="col-sm-10">
+                  <input v-model="section.title" class="form-control">
                 </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">课程</label>
-                  <div class="col-sm-10">
-                    <input v-model="section.courseId" class="form-control">
-                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">课程</label>
+                <div class="col-sm-10">
+                  <input v-model="section.courseId" class="form-control">
                 </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">大章</label>
-                  <div class="col-sm-10">
-                    <input v-model="section.chapterId" class="form-control">
-                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">大章</label>
+                <div class="col-sm-10">
+                  <input v-model="section.chapterId" class="form-control">
                 </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">视频</label>
-                  <div class="col-sm-10">
-                    <input v-model="section.video" class="form-control">
-                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">视频</label>
+                <div class="col-sm-10">
+                  <input v-model="section.video" class="form-control">
                 </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">时长</label>
-                  <div class="col-sm-10">
-                    <input v-model="section.time" class="form-control">
-                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">时长</label>
+                <div class="col-sm-10">
+                  <input v-model="section.time" class="form-control">
                 </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">收费</label>
-                  <div class="col-sm-10">
-                    <select v-model="section.charge" class="form-control">
-                      <option v-for="o in CHARGE" v-bind:value="o.key">{{o.value}}</option>
-                    </select>
-                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">收费</label>
+                <div class="col-sm-10">
+                  <select v-model="section.charge" class="form-control">
+                    <option v-for="o in CHARGE" v-bind:value="o.key">{{ o.value }}</option>
+                  </select>
                 </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">顺序</label>
-                  <div class="col-sm-10">
-                    <input v-model="section.sort" class="form-control">
-                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">顺序</label>
+                <div class="col-sm-10">
+                  <input v-model="section.sort" class="form-control">
                 </div>
+              </div>
             </form>
           </div>
           <div class="modal-footer">
@@ -138,7 +138,7 @@ export default {
     return {
       section: {},
       sections: [],
-      CHARGE:[{key:"C",value:"收费"},{key: "F",value: "免费"}]
+      CHARGE: CHARGE,
     }
   },
   mounted() {
@@ -179,14 +179,14 @@ export default {
     },
     save() {
       let _this = this;
-        // 保存校验
-        if (1 != 1
+      // 保存校验
+      if (1 != 1
           || !Validator.require(_this.section.title, "标题")
           || !Validator.length(_this.section.title, "标题", 1, 50)
           || !Validator.length(_this.section.video, "视频", 1, 200)
-        ) {
-          return;
-        }
+      ) {
+        return;
+      }
 
       _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/section/save",
           _this.section).then((response) => {
@@ -196,7 +196,7 @@ export default {
           $("#form-modal").modal('hide');
           _this.list(1)
           Toast.success("保存成功");
-        }else{
+        } else {
           Toast.warning(resp.message);
 
         }
@@ -204,7 +204,7 @@ export default {
     },
     del(id) {
       let _this = this;
-      Confirm.show("删除小节后不可恢复!",function () {
+      Confirm.show("删除小节后不可恢复!", function () {
         Loading.show();
         _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/section/delete/" + id).then((response) => {
           console.log("删除小节列表结果", response);
@@ -215,7 +215,7 @@ export default {
 
           }
         })
-    });
+      });
     }
 
   }
