4ba210e3f9cb8d34fcf82106fba3bd51ed4dd100
JiangTaoHou <925147954@qq.com>
2022-03-03 13:37:36 +0800

    网站首页开发，集成album模板

--- web/public/static/js/enums.js
+++ web/public/static/js/enums.js
@@ -6,6 +6,12 @@ COURSE_STATUS={PUBLISH:{key:"P", value:"发布"},DRAFT:{key:"D", value:"草稿"}
 FILE_USE={COURSE:{key:"C", value:"课程"},TEACHER:{key:"T", value:"讲师"}};
 SMS_USE={REGISTER:{key:"R", value:"注册"},FORGET:{key:"F", value:"忘记密码"}};
 SMS_STATUS={USED:{key:"U", value:"已使用"},NOT_USED:{key:"N", value:"未使用"}};
+PLANT_STATUS={YES:{key:"1", value:"已被领养"},NO:{key:"0", value:"未被领养"}};
+APPLY_STATUS={AGREE:{key:"1", value:"同意领养"},DISAGREE:{key:"0", value:"拒绝领养"}};
+ADOPT_STATUS={APPLY:{key:"1", value:"申请领养"},FINISHED:{key:"0", value:"领养完成"}};
+
+
+
 SECTION_CHARGE_ARRAY=[{key:"C", value:"收费"},{key:"F", value:"免费"}];
 YES_NO_ARRAY=[{key:"1", value:"是"},{key:"0", value:"否"}];
 COURSE_LEVEL_ARRAY=[{key:"1", value:"初级"},{key:"2", value:"中级"},{key:"3", value:"高级"}];
